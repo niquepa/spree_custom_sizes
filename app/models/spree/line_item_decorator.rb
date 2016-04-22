d7a2577f421fbae3a3b67bae3aa0502de9409ec8
Carlos Castillo <niquepa@gmail.com>
2016-04-22 19:45:23 -0400

    - Ajustes según currency como esta en la version inicial

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -4,39 +4,30 @@ module Spree
     alias_method :orig_update_price_from_modifier, :update_price_from_modifier
 
     def update_price_from_modifier(currency, opts)
-      #if currency
-      #  self.currency = currency
-      #  self.price = variant.price_in(currency).amount +
-      #    variant.price_modifier_amount_in(currency, opts)
-      #else
-      #  self.price = variant.price +
-      #    variant.price_modifier_amount(opts)
-      #end
-      #self.price = 230979
 
-      #area = if !opts[:height].blank? && !opts[:weight].blank?
-      #  opts[:height] * opts[:weight]
-      #else
-      #  400
-      #end
-      logger.fatal "************* ERROR ************"
-      logger.fatal "************* #{opts} ************"
-      logger.fatal "************* H=>#{opts['height']} ************"
-      logger.fatal "************* W=>#{opts['weight']} ************"
+      if currency
+        self.currency = currency
+        self.price = variant.price_in(currency).amount +
+          variant.price_modifier_amount_in(currency, opts)
+      else
+        self.price = variant.price +
+          variant.price_modifier_amount(opts)
+      end
+
       area = opts['height'].to_i * opts['weight'].to_i
       perimeter = (opts['height'].to_i + opts['weight'].to_i)*2
 
-      logger.fatal "************* AREA =>#{area} ************"
-      logger.fatal "************* PERIMETER =>#{perimeter} ************"
-
       self.price = if area > 0
-        area * variant.price
-      else
-        variant.price
-      end
+        area * self.price
+      end      
 
-      logger.fatal "************* PRICE =>#{variant.price} ************"
-      logger.fatal "************* TOTAL PRICE =>#{self.price} ************"
+      #logger.fatal "************* #{opts} ************"
+      #logger.fatal "************* H=>#{opts['height']} ************"
+      #logger.fatal "************* W=>#{opts['weight']} ************"
+      #logger.fatal "************* AREA =>#{area} ************"
+      #logger.fatal "************* PERIMETER =>#{perimeter} ************"
+      #logger.fatal "************* PRICE =>#{variant.price} ************"
+      #logger.fatal "************* TOTAL PRICE =>#{self.price} ************"
 
     end
   end
