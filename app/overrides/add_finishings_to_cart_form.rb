1091e8ec7f0407d996b99992726ad87958e4996b
Carlos Castillo <niquepa@gmail.com>
2016-05-12 14:16:52 -0400

    - Ponemos secuencia al deface para que se pinten los finishings después del job_name

--- app/overrides/add_finishings_to_cart_form.rb
+++ app/overrides/add_finishings_to_cart_form.rb
@@ -1,4 +1,5 @@
 Deface::Override.new(:virtual_path  => "spree/products/_cart_form",
                      :insert_after  => "div#job-name",
                      :partial       => "spree/products/finishings",
-                     :name          => "finishings")
+                     :name          => "finishings",
+                     :sequence      => {:after => "job_name"})
